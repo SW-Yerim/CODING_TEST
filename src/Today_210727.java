9b0557c1f46e15c19199c2d028c64980c9ee64eb
SW-Yerim <swyr159@gmail.com>
2021-07-27 10:13:48 +0900

    210727 코딩테스트 문제풀이

---
+++ src/Today_210727.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Today_210727 {
+
+    public static String solution(String[] participant, String[] completion) {
+
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!(participant[i].equals(completion[i]))) {
+                answer = participant[i];
+                break;
+            } else if (i == completion.length - 1) {
+                answer = participant[i + 1];
+            }
+        }
+
+        return answer;
+
+//        // 비슷한 좋은 코드
+//        Arrays.sort(participant);
+//        Arrays.sort(completion);
+//
+//        for (int i = 0; i < completion.length; i++) {
+//            if (!(participant[i].equals(completion[i]))) {
+//                return participant[i];
+//            }
+//        }
+//        return participant[participant.length-1];
+
+    }
+
+    public static void main(String[] args) {
+
+        // 예제 1
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"kiki", "eden"};
+        // 예제 2
+//        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+//        String[] completion = {"josipa", "filipa", "marina", "nikola"};
+        // 예제 3
+//        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+//        String[] completion = {"stanko", "ana", "mislav"};
+
+        System.out.println(solution(participant, completion));
+
+    }
+
+}
