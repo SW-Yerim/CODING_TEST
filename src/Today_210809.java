7c600c5855a14bbdcfc0413e0668179056072a37
SW-Yerim <swyr159@gmail.com>
2021-08-09 14:24:58 +0900

    210809 백준 코딩테스트 [ 평균 ]

---
+++ src/Today_210809.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Today_210809 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        int length = scan.nextInt();
+        double max = 0, sum = 0;
+
+        for (int i = 0; i < length; i++) {
+            int num = scan.nextInt();
+            sum += num;
+            if (max < num) max = num;
+        }
+
+        System.out.printf("%.2f", sum * 100 / max / length);
+
+    }
+
+}
