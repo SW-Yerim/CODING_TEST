0b68c097caff94812fcb6226d8d5f41982de91d1
SW-Yerim <swyr159@gmail.com>
2021-08-13 17:25:54 +0900

    210813 백준 코딩테스트 [ 아스키 코드 ]

---
+++ src/Today_210813.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Today_210813 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int ch = scan.next().charAt(0);
+
+        System.out.println(ch);
+
+//        String[] arr = {
+//                        "0","1","2","3","4","5","6","7","8","9",
+//                        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//                        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+//                        };
+//
+//        String input = scan.next();
+//
+//        int num = 47;
+//        for (int i = 0; i < arr.length; i++) {
+//            num++;
+//            if (num == 58) num = 65;
+//            if (num == 91) num = 97;
+//
+//            if (input.equals(arr[i])) {
+//                System.out.println(num);
+//            }
+//        }
+
+    }
+}
