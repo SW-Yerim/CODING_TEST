50aca1d7fa59d4a12eb9615b305110367b64cfe9
SW-Yerim <swyr159@gmail.com>
2021-09-02 09:48:17 +0900

    210902 백준 코딩테스트 [ 크로아티아 알파벳 ]

---
+++ src/Today_210902.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Today_210902 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+
+        if (s.contains("c=")) s = s.replaceAll("c=", "A");
+        if (s.contains("c-")) s = s.replaceAll("c-", "A");
+        if (s.contains("dz=")) s = s.replaceAll("dz=", "A");
+        if (s.contains("d-")) s = s.replaceAll("d-", "A");
+        if (s.contains("lj")) s = s.replaceAll("lj", "A");
+        if (s.contains("nj")) s = s.replaceAll("nj", "A");
+        if (s.contains("s=")) s = s.replaceAll("s=", "A");
+        if (s.contains("z=")) s = s.replaceAll("z=", "A");
+
+        int result = s.length();
+
+        System.out.println(result);
+
+    }
+}
